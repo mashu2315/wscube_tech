ed681cf66078adfadce3076dc39e15919c6fbaa0
Ashutosh Maurya <mauryaashutosh1983@gmail.com>
2025-03-26 13:57:14 +0530

    fixing navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,47 +14,47 @@ const Footer = () => {
         <h1 className='w-full text-3xl font-bold text-black'>WsCube Tech.</h1>
         <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
         <div className='flex justify-between md:w-[75%] my-6'>
-          <FaFacebookSquare size={30} />
-          <FaInstagram size={30} />
-          <FaTwitterSquare size={30} />
-          <FaGithubSquare size={30} />
+          <FaFacebookSquare size={30}  className=' hover:text-blue-200 hover:scale-105'/>
+          <FaInstagram size={30}  className=' hover:text-blue-200 hover:scale-105'/>
+          <FaTwitterSquare size={30} className=' hover:text-blue-200 hover:scale-105' />
+          <FaGithubSquare size={30}  className=' hover:text-blue-200 hover:scale-105'/>
         </div>
       </div>
       <div className='lg:col-span-2 flex justify-between mt-6'>
         <div>
           <h6 className='font-medium text-black'>Solutions</h6>
           <ul>
-            <li className='py-2 text-sm'>Analytics</li>
-            <li className='py-2 text-sm'>Marketing</li>
-            <li className='py-2 text-sm'>Commerce</li>
-            <li className='py-2 text-sm'>Insights</li>
+            <li className='py-2 text-sm hover:text-blue-200 hover:scale-105 '>Analytics</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Marketing</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Commerce</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Insights</li>
           </ul>
         </div>
         <div>
           <h6 className='font-medium text-black'>Support</h6>
           <ul>
-            <li className='py-2 text-sm'>Pricing</li>
-            <li className='py-2 text-sm'>Documentation</li>
-            <li className='py-2 text-sm'>Guides</li>
-            <li className='py-2 text-sm'>API Status</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Pricing</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Documentation</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Guides</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>API Status</li>
           </ul>
         </div>
         <div>
           <h6 className='font-medium text-black'>Company</h6>
           <ul>
-            <li className='py-2 text-sm'>About</li>
-            <li className='py-2 text-sm'>Blog</li>
-            <li className='py-2 text-sm'>Jobs</li>
-            <li className='py-2 text-sm'>Press</li>
-            <li className='py-2 text-sm'>Careers</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>About</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Blog</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Jobs</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Press</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Careers</li>
           </ul>
         </div>
         <div>
           <h6 className='font-medium text-black'>Legal</h6>
           <ul>
-            <li className='py-2 text-sm'>Claim</li>
-            <li className='py-2 text-sm'>Policy</li>
-            <li className='py-2 text-sm'>Terms</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Claim</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Policy</li>
+            <li className='py-2 text-sm  hover:text-blue-200 hover:scale-105'>Terms</li>
           </ul>
         </div>
       </div>
